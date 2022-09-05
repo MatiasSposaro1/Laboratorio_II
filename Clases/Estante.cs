7c7b6b5422ee719073a6568b63bd296ea4350cbb
MatiasSposaro1 <matiassposarobeccan@gmail.com>
2022-09-05 20:58:26 -0300

    Agregar archivos de proyecto.

---
+++ Clases/Estante.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_C02___La_estantería.Clases
+{
+    public class Estante
+    {
+        private Producto[] _productos;
+        private int _ubicacion;
+
+        private Estante(int capacidad)
+        {
+            _productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this._ubicacion = ubicacion;
+        }
+
+        public Producto[] Productos
+        {
+            get { return _productos; }
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Producto p in e.Productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(p));
+            }
+            return sb.ToString();//error aca
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool isAlready = false;
+            foreach (Producto p2 in e.Productos)
+            {
+                if (p == p2) //error aca
+                {
+                    isAlready = true;
+                    break;
+                }
+            }
+            return isAlready;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool canAdd = false;
+            if (!(e == p)) //error aca
+            {
+                int size = e.Productos.Length;
+                for (int i = 0; i < size; i++)
+                {
+                    if (e.Productos[i] is null)
+                    {
+                        e.Productos[i] = p;
+                        canAdd = true;
+                        break;
+                    }
+                }
+            }
+
+            return canAdd;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if (e == p)
+            {
+                int cont = 0;
+                foreach (Producto p2 in e.Productos)
+                {
+                    if (p2 == p)
+                    {
+                        e.Productos[cont] = null;
+                    }
+                    cont++;
+                }
+            }
+            return e;
+        }
+    }
+}
---
+++ Clases/Producto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_C02___La_estantería.Clases
+{
+     public class Producto
+    {
+        private string _codigoDeBarra;
+        private string _marca;
+        private float _precio;
+
+        public Producto(string codigoDeBarra, string marca, float precio)
+        {
+            _codigoDeBarra = codigoDeBarra;
+            _marca = marca;
+            _precio = precio;
+        }
+
+        #region GETTERS
+        public string GetMarca()
+        {
+            return _marca;
+        } 
+
+        public float GetPrecio()
+        {
+            return _precio;
+        }
+
+        #endregion
+        #region SOBRECARGA
+        public static explicit operator string(Producto p)
+        {
+            return p._codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p._marca == marca;
+        }
+        public static bool operator != (Producto p,string marca)
+        {
+            return !(p._marca == marca);
+        }
+
+
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            return p1 == p2;
+        }
+        
+        public static bool operator != (Producto p1,Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        #endregion
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Codigo de Barra:{p._codigoDeBarra}|Marca:{p._marca}      Precio:${p._precio}");
+            sb.AppendLine("_____________");
+
+            return sb.ToString();
+        }
+
+    }
+}
